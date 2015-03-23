4bcb536791b17b1460568055026e86de0ac0ade9
Joppe Aarts <joppe@apestaartje.info>
2015-03-23 22:50:39 +0100

    initial commit

---
+++ js/foo.js
@@ -0,0 +1,5 @@
+export class Foo {
+    constructor() {
+        this.es6 = 'yay';
+    }
+}
---
+++ js/foo2.js
@@ -0,0 +1,18 @@
+System.register([], function (_export) {
+    var _classCallCheck, Foo;
+
+    return {
+        setters: [],
+        execute: function () {
+            "use strict";
+
+            _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
+            Foo = _export("Foo", function Foo() {
+                _classCallCheck(this, Foo);
+
+                this.es6 = "yay";
+            });
+        }
+    };
+});
